202c37394b147ad467f25cc4e056a4ef823c5734
Mihail Baranow <mihailbaranow2018@gmail.com>
2023-07-19 22:47:57 +0300

    feature/popit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openPopup() {
+    document.getElementById("popup").classList.remove("popup_opened"); 
+  }
+  function closePopup() {
+    document.getElementById("popup").classList.add("popup_opened");
+  }
