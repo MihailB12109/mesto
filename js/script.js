857e760dfa20352048c98de1aeca5825a3b5caf9
Mihail Baranow <mihailbaranow2018@gmail.com>
2023-07-21 13:41:05 +0300

    feature/JSpopup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+const openPopupButton = document.querySelector("#openPopup");
+const savePopupButton = document.querySelector("#savePopup");
+const closePopupButton = document.querySelector("#closePopup")
+const popup = document.querySelector("#popup");
+const youName = document.querySelector("#userName");
+const about = document.querySelector("#userData");
+
+let nameValue = document.querySelector("#realName");
+let dataValue = document.querySelector("#realData");
+
 function openPopup() {
-    document.getElementById("popup").classList.remove("popup_opened"); 
-  }
-  function closePopup() {
+    document.getElementById("popup").classList.remove("popup_opened");
+    youName.value = nameValue.textContent;
+    about.value = dataValue.textContent;
+}
+
+function savePopup() {
     document.getElementById("popup").classList.add("popup_opened");
-  }
+    nameValue.textContent = youName.value;
+    dataValue.textContent = about.value;
+}
+
+function closePopup() {
+    document.getElementById("popup").classList.add("popup_opened");
+}
+
+
+
+openPopupButton.addEventListener("click", openPopup);
+savePopupButton.addEventListener("click", savePopup);
+closePopupButton.addEventListener("click", closePopup);
+
