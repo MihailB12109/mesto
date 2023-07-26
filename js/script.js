802ac42db3a813151028c65c33d1e44c195ec97e
Mihail Baranow <mihailbaranow2018@gmail.com>
2023-07-26 21:48:32 +0300

    modified:   blocks/avatar/avatar.css 	modified:   blocks/elements/elements.css 	modified:   blocks/header/header.css 	modified:   blocks/logo/logo.css 	modified:   blocks/page/page.css 	modified:   blocks/popup/_opened/popup_opened.css 	modified:   blocks/popup/popup.css 	modified:   blocks/profile-info/__edit-button/profile-info__edit-button.css 	modified:   blocks/profile-info/__icon/profile-info__icon.css 	modified:   blocks/profile-info/__name/profile-info__name.css 	modified:   blocks/profile-info/__subname/profile-info__subname.css 	modified:   blocks/profiles/profiles.css 	modified:   index.html 	modified:   js/script.js 	modified:   vendor/fonts/fonts.css

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 const openPopupButton = document.querySelector("#openPopup");
 const savePopupButton = document.querySelector("#savePopup");
 const closePopupButton = document.querySelector("#closePopup")
-const popup = document.querySelector("#popup");
 const youName = document.querySelector("#userName");
 const about = document.querySelector("#userData");
+const popupElement = document.getElementById("popup");
 
 let nameValue = document.querySelector("#realName");
 let dataValue = document.querySelector("#realData");
 
 function openPopup() {
-    document.getElementById("popup").classList.remove("popup_opened");
+    popupElement.classList.add("popup_opened");
+    popupElement.classList.remove("popup")
     youName.value = nameValue.textContent;
     about.value = dataValue.textContent;
 }
 
 function savePopup() {
-    document.getElementById("popup").classList.add("popup_opened");
+    popupElement.classList.remove("popup_opened");
+    popupElement.classList.add("popup");
     nameValue.textContent = youName.value;
     dataValue.textContent = about.value;
 }
 
 function closePopup() {
-    document.getElementById("popup").classList.add("popup_opened");
+    popupElement.classList.add("popup");
 }
 
 
